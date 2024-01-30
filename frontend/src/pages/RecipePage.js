524b50498e5b17950a9b4a2962b4ef350cd7c15a
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2024-01-30 13:30:16 +0200

    Ainesosia + komponentit

--- frontend/src/pages/RecipePage.js
+++ frontend/src/pages/RecipePage.js
@@ -2,11 +2,46 @@ import {useState, useEffect} from 'react';
 //Uniikit URL:it
 //poistopainike + varmistus
 const Recipe = () =>{
+    const [ingredients, setIngredients] = useState([
+        {"quantity": "1 dl", "ingredient":"flour"}, 
+        {"quantity": "2 tsp", "ingredient":"salt"},
+        {"quantity": "1 tbsp", "ingredient":"pepper"}
+        //esimerkkilista kehitystä varten
+    ]);
 
     return(
         <div>
+            <RecipeHead />
+            <RecipeIngredients ingredients={ingredients}/>
+            <RecipeSteps />
         </div>
     );
 }
 
-export  {Recipe};
+const RecipeHead = (props) =>{
+    return(
+        <div></div>
+    );
+}
+
+const RecipeIngredients = (props)=>{
+
+    const ingredientList = props.ingredients.map((ing,i) =>{
+        return <tr key={i}><th>{ing.quantity}</th><td>{ing.ingredient}</td></tr>
+    });
+
+    return(
+        <table>
+            <caption>Ingredients</caption>
+            {ingredientList}
+        </table>
+    );
+}
+
+const RecipeSteps = (props) =>{
+    return(
+        <div></div>
+    );
+}
+
+export  {Recipe, RecipeHead, RecipeIngredients, RecipeSteps};
