38a360abdc59e548907512ed468b34b1a20615c6
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2024-01-30 14:29:03 +0200

    RecipePage alkua

--- frontend/src/pages/RecipePage.js
+++ frontend/src/pages/RecipePage.js
@@ -1,26 +1,44 @@
 import {useState, useEffect} from 'react';
 //Uniikit URL:it
 //poistopainike + varmistus
+
 const Recipe = () =>{
+    
+    //esimerkkilistoja kehitystä varten
     const [ingredients, setIngredients] = useState([
         {"quantity": "1 dl", "ingredient":"flour"}, 
         {"quantity": "2 tsp", "ingredient":"salt"},
         {"quantity": "1 tbsp", "ingredient":"pepper"}
-        //esimerkkilista kehitystä varten
+    ]);
+    const [steps, setSteps] = useState(["Eka kohta", "Toka kohta", "Kolmas kohta", "jne"]);
+    const [reviews, setReviews] = useState([
+        {"username":"joku", "rating":"5/5", "text":"Oli kyllä mainio!"},
+        {"username":"t", "rating":"2/5", "text":"Liian helppo :("},
+        {"username":"möh", "rating":"4/4", "text":"Maukas"}
     ]);
 
     return(
-        <div>
+        <div className='RecipeContainer'>
             <RecipeHead />
             <RecipeIngredients ingredients={ingredients}/>
-            <RecipeSteps />
+            <RecipeSteps steps={steps}/>
+            <RecipeReviews reviews={reviews}/>
         </div>
     );
 }
 
+//Palauttaa muotoillun 
 const RecipeHead = (props) =>{
+    //HUOM! Asettelu gridiin keskelle!
     return(
-        <div></div>
+        <div>
+            <h1>Name</h1>
+            <p>Description</p>
+            <div>kuva sivummalle</div>
+            <p>Creator <i>Created</i></p>
+            <div>Star-rating, duration</div>
+            <div>Keywords_list</div> <br/>
+        </div>
     );
 }
 
@@ -33,15 +51,34 @@ const RecipeIngredients = (props)=>{
     return(
         <table>
             <caption>Ingredients</caption>
-            {ingredientList}
+            <tbody>{ingredientList}</tbody>
         </table>
     );
 }
 
 const RecipeSteps = (props) =>{
+    const steps = props.steps.map((step, i) =>{
+        return <li key={i}>{step}</li>
+    });
+
     return(
-        <div></div>
+        <div>
+            <ol>{steps}</ol>
+        </div>
     );
 }
 
-export  {Recipe, RecipeHead, RecipeIngredients, RecipeSteps};
+const RecipeReviews = (props) =>{
+    //HUOM! Mieti asettelu!
+    const reviews = props.reviews.map((r, i) =>{
+        return <tr key={i}><th>{r.rating}</th><td>{r.username}:</td><td><i>{r.text}</i></td></tr>
+    });
+
+    return(
+        <table>
+            <tbody>{reviews}</tbody>
+        </table>
+    );
+}
+
+export  {Recipe, RecipeHead, RecipeIngredients, RecipeSteps, RecipeReviews};
