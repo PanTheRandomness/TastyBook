5ce90b7a979801097b08dde63b86ab65e8ba63b9
IirisHa <130594249+IirisHa@users.noreply.github.com>
2024-02-16 16:05:24 +0200

    RecioeVIews-testit toimii

--- frontend/src/api/apiTests/recipeApi.test.js
+++ frontend/src/api/apiTests/recipeApi.test.js
@@ -1,4 +1,5 @@
 const { getRecipeRoutes } = require("../recipeApi");
+const { getRecipeViews } = require("../recipeApi");
 const BASE_URL = "http://localhost:3004";
 
 describe("getRecipeRoutes", () => {
@@ -45,3 +46,51 @@ describe("getRecipeRoutes", () => {
         await expect(getRecipeRoutes()).rejects.toThrow("Fetch failed");
     });
 });
+
+//RecipeViews testit:
+
+describe("getRecipeViews", () => {
+    beforeEach(() => {
+        global.fetch = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should fetch recipeviews without token", async () => {
+        fetch.mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce({ hashes: [{ id: 1, hash: "123"}, { id: 2, hash: "234" }]}),
+        });
+
+        const response = await getRecipeViews();
+
+        expect(fetch).toHaveBeenCalledWith(`${BASE_URL}/api/recipes`);
+        expect(response.loggedIn).toBeUndefined();
+        expect(response.hashes).toEqual( [{ id: 1, hash: "123"}, { id: 2, hash: "234" }]);
+    });
+
+    it("should fetch recipeviews with token", async () => {
+        const token = "mockToken";
+        fetch.mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce({ loggedIn: true, hashes: [{ id: 1, hash: "123"}, { id: 2, hash: "234" }]}),
+        });
+
+        const response = await getRecipeViews(token);
+
+        expect(fetch).toHaveBeenCalledWith(`${BASE_URL}/api/recipes`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        expect(response.loggedIn).toBe(true);
+        expect(response.hashes).toEqual([{ id: 1, hash: "123"}, { id: 2, hash: "234" }]);
+    });
+
+    it("should throw an error if fetch fails", async () => {
+        global.fetch.mockImplementationOnce(() => Promise.reject(new Error("Fetch failed")));
+
+        await expect(getRecipeViews()).rejects.toThrow("Fetch failed");
+    });
+});
+
