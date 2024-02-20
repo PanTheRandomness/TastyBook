4298961a404ab8833a0d7d65dda3c42cf55160e5
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2024-02-20 14:09:22 +0200

    Voi muokata tai poistaa vain oamt reseptit

--- frontend/src/components/EllipsisMenu.js
+++ frontend/src/components/EllipsisMenu.js
@@ -1,57 +1,60 @@
 import { useState, useEffect, useRef } from 'react';
+import { useUser } from '../customHooks/useUser';
 
 const EllipsisMenu = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const user = useUser();
+  const creator = props.creator;
   
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    //Alla olevat kaksi (ja useRef) mahdollistavat menun sulkemisen klikkaamalla sen ulkopuolelta!
-    const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-          setIsOpen(false);
-        }
-    };
+  //Alla olevat kaksi (ja useRef) mahdollistavat menun sulkemisen klikkaamalla sen ulkopuolelta!
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
 
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
-    const handleEditClick = () => {
-        console.log('Edit recipe selected...');
-        setIsOpen(false);
-        props.onEdit();
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
     };
+  }, []);
 
-    const handleDeleteClick = () => {
-        setIsOpen(false);
-        if(window.confirm("Are you certain you want to delete this recipe? Deletion cannot be undone.")){
-            props.onDelete();
-        }
-    };
+  const handleEditClick = () => {
+    console.log('Edit recipe selected...');
+    setIsOpen(false);
+    props.onEdit();
+  };
+
+  const handleDeleteClick = () => {
+    setIsOpen(false);
+    if(window.confirm("Are you certain you want to delete this recipe? Deletion cannot be undone.")){
+      props.onDelete();
+    }
+  };
   
-    return (
-      <div className="ellipsis-menu">
-        <div className="ellipsis" onClick={toggleMenu}>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-        </div>
-        {isOpen && (
-          <div className="dropdown" ref={dropdownRef}>
-            <ul>
-              <li onClick={handleEditClick}>Edit recipe</li>
-              <li onClick={handleDeleteClick} style={{color:'red'}}>Delete recipe</li>
-            </ul>
-          </div>
-        )}
+  return (
+    <div className="ellipsis-menu">
+      <div className="ellipsis" onClick={toggleMenu}>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
       </div>
-    );
+      {isOpen && (
+        <div className="dropdown" ref={dropdownRef}>
+          <ul>
+            { user.username == creator ? <li onClick={handleEditClick}>Edit recipe</li>:null}
+            { user.username == creator ? <li onClick={handleDeleteClick} style={{color:'red'}}>Delete recipe</li>:null}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EllipsisMenu;
