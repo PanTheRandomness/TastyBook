354b808e4e5e64fe1d05a1f87b9a9ce166079a01
Pan T. <107498260+PanTheRandomness@users.noreply.github.com>
2024-02-20 13:17:38 +0200

    Edelleen kommenttia

--- frontend/src/pages/pagesTests/RecipePage.test.js
+++ frontend/src/pages/pagesTests/RecipePage.test.js
@@ -23,10 +23,10 @@ describe('RecipePage component', () => {
       
         // Renderöi reseptisivu
         render(
+            /*Tähän testi jää jumiin, ei osaa renderöidä resepti-komponenttia */
             <MemoryRouter initialEntries={['/recipe/test']}>
                 <Route path="/recipe/:route">
-                    {/*Tähän testi jää jumiin, ei osaa renderöidä resepti-komponenttia */}
-                    <RecipePage />
+                    <Recipe />
                 </Route>
             </MemoryRouter>
         );
