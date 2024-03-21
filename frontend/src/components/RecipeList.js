9114e3574ffc4d549a885ac8e15ae2de22ed09ba
hhannaee <hannakarkkainen@hotmail.com>
2024-03-21 16:35:57 +0200

    muutokset

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -4,35 +4,15 @@ import '../Styles/RecipeView.css';
 import RecipeView from "../components/recipeView";
 import { NavLink } from "react-router-dom";
 
-
 const BASE_URL = 'http://localhost:3004/api/recipes';
 
 const RecipeList = () => {
   const user = useUser();
-  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [myRecipes, setMyRecipes] = useState([]);
-  const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
-  /*  const fetchFavoriteRecipes = async () => {
-      try {
-        if (!user || !user.id) {
-          throw new Error('User ID not available');
-        }
-
-        const response = await fetch(`${BASE_URL}/favorites/${user.id}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch favorite recipes');
-        }
-        const data = await response.json();
-        setFavoriteRecipes(data.recipes);
-      } catch (error) {
-        setError('Error fetching favorite recipes');
-      }
-    };
- */
     const fetchMyRecipes = async () => {
       try {
         if (!user || !user.username) {
@@ -52,17 +32,40 @@ const RecipeList = () => {
       }
     };
 
-    if (user && user.username) { // Lisätty tarkistus käyttäjänimen olemassaolosta
-      fetchMyRecipes(); // Muutettu hakemaan käyttäjän reseptejä käyttäjänimen perusteella
-      if (user.id) { // Lisätty tarkistus käyttäjä ID:n olemassaolosta
-      //  fetchFavoriteRecipes();
-      } else {
-        setLoading(false);
-      }
+    if (user && user.username) {
+      fetchMyRecipes();
     } else {
       setLoading(false);
     }
   }, [user]);
+
+  const addToFavorites = async (recipeId) => {
+    try {
+      const response = await fetch('http://localhost:3004/api/favourite', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}` 
+        },
+        body: JSON.stringify({ recipeId })
+      });
+      if (!response.ok) {
+        throw new Error('Failed to add recipe to favorites');
+      }
+      // Päivitä suosikkireseptien tila uudella reseptin tunnuksella
+      setMyRecipes(prevRecipes => {
+        const updatedRecipes = prevRecipes.map(recipe => {
+          if (recipe.id === recipeId) {
+            return { ...recipe, isFavorite: true };
+          }
+          return recipe;
+        });
+        return updatedRecipes;
+      });
+    } catch (error) {
+      console.error('Error adding recipe to favorites:', error.message);
+    }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -73,21 +76,23 @@ const RecipeList = () => {
   }
 
   return (
-      <div className="recipe-list-container">
-        <h2>My Recipes</h2>
-        {myRecipes.length > 0 ? (
-          <ul className='recipeViewContainer'>
-            {myRecipes.map((recipe, index) => (
-              <div key={index}>
-                <NavLink className={"recipeView"} to={`/recipe/${recipe.hash}`}>
-                  <RecipeView key={recipe.id} recipe={recipe} />
-                </NavLink>
-              </div>
-            ))}
-          </ul>
-        ) : (
-          <div>No recipes found.</div>
-        )}
+    <div className="recipe-list-container">
+      <h2>My Recipes</h2>
+      {myRecipes.length > 0 ? (
+        <ul className='recipeViewContainer'>
+          {myRecipes.map((recipe, index) => (
+            <div key={index}>
+              <NavLink className={"recipeView"} to={`/recipe/${recipe.hash}`}>
+                <RecipeView key={recipe.id} recipe={recipe} />
+              </NavLink>
+              {/* Renderöi lisää suosikiksi -painike vain, jos reseptiä ei ole jo lisätty suosikkeihin */}
+              {!recipe.isFavorite && <button onClick={() => addToFavorites(recipe.id)}>Add to Favorites</button>}
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div>No recipes found.</div>
+      )}
     </div>
   );
 };
